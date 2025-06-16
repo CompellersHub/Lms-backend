9d773b259e84653d550e64bc22f22f9817835329
shuazafen <olomoshuaomozafen@gmail.com>
2025-06-16 23:32:33 +0100

    utils.py

--- user/utils/__init__.py
+++ user/utils/__init__.py
@@ -1,4 +1,4 @@
 # users/utils/__init__.py
-from .token_utils import create_jwt_token_pair, create_access_token, decode_jwt_token
+from .token_utils import create_jwt_tokens, create_access_token, decode_jwt_token
 
-__all__ = ['create_jwt_token_pair', 'create_access_token', 'decode_jwt_token']
+__all__ = ['create_jwt_tokens', 'create_access_token', 'decode_jwt_token']

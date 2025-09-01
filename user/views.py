4f42708ce0d1c25a6399dddd91072e419c3f29c6
shuazafen <olomoshuaomozafen@gmail.com>
2025-09-01 09:49:33 +0100

    new tokens

--- user/views.py
+++ user/views.py
@@ -154,7 +154,7 @@ class GoogleLoginView(APIView):
             return Response({
                 'access': tokens['access'],
                 'refresh': tokens['refresh'],
-                'user_info': serializer.data,
+                'user': serializer.data,
                 'message': "Login Successful"
             }, status=status.HTTP_200_OK)
 

7845d2d0ae986da967346f5a225ae2c9ac8ae9c5
shuazafen <olomoshuaomozafen@gmail.com>
2025-10-11 20:39:13 +0100

    docker and new pc push

--- courses/views.py
+++ courses/views.py
@@ -1561,8 +1561,8 @@ class SendTemplateToListAPIView(APIView):
         brevo_api_key = settings.BREVO_API_KEY
         sender_email = settings.DEFAULT_FROM_EMAIL
         sender_name = "Titans Careers"
-        list_id = 8 # Specific list ID
-        template_id = 36  # Specific template ID
+        list_id = 7 # Specific list ID
+        template_id = 42  # Specific template ID
     
         # Get all contacts from the specified list
         contacts = self.get_brevo_contacts_in_list(brevo_api_key, list_id)
